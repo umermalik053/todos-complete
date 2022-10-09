258e22dd8b5fe5ee9323f6deed1c9f243b7e6d06
umermalik053 <um4273209@gmail.com>
2022-10-09 12:38:35 -0700

    update

--- app.js
+++ app.js
@@ -1,36 +1,52 @@
 
 
-let api = 'https://amethyst-caterpillar-boot.cyclic.app/'
-function ClassName(){
-    // Make it 
+// let api = 'https://amethyst-caterpillar-boot.cyclic.app/'
+let api = 'https://easy-teal-salamander-hose.cyclic.app';
+function Classid() {
+    let id = document.querySelector("#classid").value
+    axios.post('/id', {
+        classid: id
+
+
+    })
+        .then(function (response) {
+            console.log(response)
+        })
+
+
+
+
 }
 function list() {
     document.querySelector("#addData").innerHTML = ""
     let data = document.querySelector("#addData")
     // console.log(data);
-    axios.post(`${api}todo`, {
+    axios.post(`${api}/todo`, {
         text: data.value
     })
         .then(function (response) {
             document.querySelector("#result").innerHTML = ""
             console.log(response.data)
             response.data.data.map(eachTodo => {
-                renderItems(eachTodo)                         
+                renderItems(eachTodo)
             })
         })
         .catch(function (error) {
             // document.querySelector("#Wdata").innerHTML = error
             console.log(error);
         })
-        data.value = ''        
+    data.value = ''
 }
 function allList() {
-    axios.get(`${api}list`)
+    axios.get(`${api}/todos`)
         .then(function (response) {
-            document.querySelector("#result").innerHTML = ""
-            console.log(response.data)
+            // document.querySelector("#result").innerHTML = "Data";
+            // console.log(response.data)
             response.data.data.map(eachTodo => {
-                renderItems(eachTodo)
+                document.querySelector("#result").innerHTML += eachTodo.text
+                document.querySelector("#result").innerHTML += "<br>"
+                console.log("eachTodo",eachTodo);
+                // renderItems(eachTodo)
             })
         })
         .catch(function (error) {
@@ -40,8 +56,8 @@ function allList() {
 function removelist() {
     let code = prompt("Enter Password ")
     if (code === "Malik") {
-        axios.delete(`${api}remove`)
-        document.querySelector("#addData").value=""
+        axios.delete(`${api}/todos`)
+        document.querySelector("#addData").value = ""
         renderItems()
     } else alert("Worng Password")
 }
@@ -53,7 +69,7 @@ function renderItems(item) {
     </li>
         </ul>`
         document.querySelector("#result").innerHTML += todo
-    }else{
+    } else {
         document.querySelector("#result").innerHTML = ''
     }
 
